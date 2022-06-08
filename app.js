43c31ed530e1ee6cd21ea8f0ce2f4ed0ed86cb6e
TomasCarranzaC <tomascarranzac@gmail.com>
2022-06-08 19:29:34 +0000

    Domain Hack Final

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var pron = ["los", "nuestros"];
-  var adj = ["geniales", "grandes"];
-  var noun = ["telecom", "caracoles", "desafio", "internet", "repelus"];
-  var ext = [".es", ".com", ".net", ".us", ".io"];
+const pron = ["los", "nuestros"];
+  const adj = ["geniales", "grandes"];
+  const noun = ["telecom", "caracoles", "desafio", "internet", "repelus"];
+  const ext = [".es", ".com", ".net", ".us", ".io"];
   for(var item of pron){
       for(var item1 of adj){
           for (var item2 of noun){
                 for (var item3 of ext){
-                    if (item2.slice(item2.length-item3.length,item2.length).includes(item3.slice(1,item3.length))){
+                    if (item2.substring(item2.length-item3.length).includes(item3.substring(1))){
                         //console.log(item2.indexOf(item3.slice(1,item3.length)));
                         console.log(item + item1 + item2.slice(0,item2.length+1-item3.length) + item3);
                     }
@@ -15,3 +15,5 @@ var pron = ["los", "nuestros"];
                 }
                 }
           }
+      }
+  }

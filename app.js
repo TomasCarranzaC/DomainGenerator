6b92ae329362aacdf2813625f32d9112dc358f04
TomasCarranzaC <tomascarranzac@gmail.com>
2022-06-08 18:53:51 +0000

    Domain Hack

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-let pronombre = ["la", "nuestra"];
-let adj = ["genial", "gran"];
-let nombre = ["comadreja", "jirafa"];
-let dominio = [".com", ".net",".es"];
-
-
-for (let i=0; i<pronombre.length; i++) { 
-
-    for (let j=0; j<nombre.length; j++) {
-
-        for (let p=0; p<adj.length; p++) {
-            for (let r= 0; r< dominio.length; r++) {
-
-                console.log(pronombre[i]+adj[p]+nombre[j]+dominio[r]);
-               }
-
-           }
-       }
-    }
+var pron = ["los", "nuestros"];
+  var adj = ["geniales", "grandes"];
+  var noun = ["telecom", "caracoles", "desafio"];
+  var ext = [".es", ".com", ".net", ".us", ".io"];
+  for(var item of pron){
+      for(var item1 of adj){
+          for (var item2 of noun){
+                for (var item3 of ext){
+                    if (item2.slice(item2.length-item3.length,item2.length).includes(item3.slice(1,item3.length))){
+                        //console.log(item2.indexOf(item3.slice(1,item3.length)));
+                        console.log(item + item1 + item2.slice(0,item2.length+1-item3.length) + item3);
+                    }
+                    else{
+                    console.log(item + item1 + item2 + item3);
+                }
+                }
+          }
